464b962139c8f2fab9ae7307a7b1188d7a9a3c8e
tianbingyao22 <2242378609@qq.com>
2022-08-15 15:57:44 +0800

    完成图片上传的功能

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import PageTools from '@/components/PageTools'
 import UploadExcel from '@/components/UploadExcel'
+import UploadImg from '@/components/UploadImg'
 // 注册全局组件只需要引入，并在数组中写入组件即可
-const components = [PageTools, UploadExcel]
+const components = [PageTools, UploadExcel, UploadImg]
 export default {
   install(Vue) {
     components.forEach((component) => {
